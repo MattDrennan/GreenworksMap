6d77464dfe9903063008d789530552cac522ae7c
Kashai <sweetpea617@outlook.com>
2021-11-05 18:25:49 -0400

    "added HttpServlet extension to the controller docs"

--- EcoMap/src/main/java/com/GREENWORKS/controller/FilterPillars.java
+++ EcoMap/src/main/java/com/GREENWORKS/controller/FilterPillars.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.LinkedList;
 
-public class FilterPillars {
+public class FilterPillars extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
 
--- EcoMap/src/main/java/com/GREENWORKS/controller/ShowPillars.java
+++ EcoMap/src/main/java/com/GREENWORKS/controller/ShowPillars.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class ShowPillars{
+public class ShowPillars extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
