df1d512c31ab2d8964f9cb5d067f21363829c3a6
EthanNValencia <ejnephew@yahoo.com>
2022-01-19 13:12:20 -0500

    I am moving the database related constants to their own DatabaseConstants file. I also updated the Intent: Organization

--- EcoMap/src/main/java/com/GREENWORKS/eco/DatabaseConstants.java
+++ EcoMap/src/main/java/com/GREENWORKS/eco/DatabaseConstants.java
@@ -10,9 +10,9 @@ package com.GREENWORKS.eco;
  * PASSWORD: Change this to your database password.
  */
 public class DatabaseConstants {
-    private static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:8889/ecomap"; 
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
-    private static final String MAX_POOL = "250";
+    public static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DATABASE_URL = "jdbc:mysql://localhost:8889/ecomap"; 
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+    public static final String MAX_POOL = "250";
 }
--- EcoMap/src/main/java/com/GREENWORKS/eco/MysqlConnect.java
+++ EcoMap/src/main/java/com/GREENWORKS/eco/MysqlConnect.java
@@ -5,41 +5,48 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+/***
+ * This class houses methods that handle database connection. 
+ */
 public class MysqlConnect {
     // Init database variables
-    private static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:8889/ecomap";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
-    private static final String MAX_POOL = "250";
 
     // Init connection object
     private Connection connection;
     // Init properties object
     private Properties properties;
 
-    // Create properties
+    /***
+     * Database properties are assigned to the properties instance variable in this method. 
+     * @return Returns the properties object with set properties. 
+     */
     private Properties getProperties()
     {
         if (properties == null)
         {
             properties = new Properties();
-            properties.setProperty("user", USERNAME);
-            properties.setProperty("password", PASSWORD);
-            properties.setProperty("MaxPooledStatements", MAX_POOL);
+            properties.setProperty("user", DatabaseConstants.USERNAME);
+            properties.setProperty("password", DatabaseConstants.PASSWORD);
+            properties.setProperty("MaxPooledStatements", DatabaseConstants.MAX_POOL);
         }
         return properties;
     }
 
-    // Connect database
+    /***
+     * This method performs a null check on the connection object. If at the point of method
+     * call the connection object is null then the connection object will have its variables
+     * fulfilled. If the connection object is not null then it can be assumed that the 
+     * connection object has already had its variables fulfilled. 
+     * @return Returns a connection object that is not null. 
+     */
     public Connection connect()
     {
         if (connection == null)
         {
             try
             {
-                Class.forName(DATABASE_DRIVER);
-                connection = DriverManager.getConnection(DATABASE_URL, getProperties());
+                Class.forName(DatabaseConstants.DATABASE_DRIVER);
+                connection = DriverManager.getConnection(DatabaseConstants.DATABASE_URL, getProperties());
             }
             catch (ClassNotFoundException | SQLException e)
             {
@@ -49,7 +56,10 @@ public class MysqlConnect {
         return connection;
     }
 
-    // Disconnect database
+    /***
+     * This method handles the requirements for disconnecting from the database. Disconnecton is achieved by 
+     * closing the connection and then nullifying the connection. 
+     */
     public void disconnect()
     {
         if (connection != null)
@@ -65,4 +75,33 @@ public class MysqlConnect {
             }
         }
     }
+
+    /***
+     * This is an accessor method for the connection instance variable. This should not be used 
+     * outside of unit testing. 
+     * @return Returns the connection object. 
+     */
+    public Connection getConnection() {
+        return connection;
+    }
+
+    /***
+     * This is a mutator method for the connection instance variable. This should not be used
+     * outside of unit testing. 
+     * @param connection
+     */
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    /***
+     * This is a mutator method for the properties instance variable. This should not be used
+     * outside of unit testing. 
+     * @param properties
+     */
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    
 }
