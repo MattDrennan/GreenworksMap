60a01ad206f914e6a2c9c9a05b8cda7e2c57d944
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2022-01-19 20:26:16 -0500

    Delete Location

--- EcoMap/src/main/java/com/GREENWORKS/eco/EditLocation.java
+++ EcoMap/src/main/java/com/GREENWORKS/eco/EditLocation.java
@@ -21,9 +21,17 @@ public class EditLocation extends HttpServlet {
     {
         // Get ID of location
         String locationID = request.getParameter("locationID");
+        String deleteChoose = request.getParameter("deleteChoose");
+        String editChoose = request.getParameter("editChoose");
 
         // Statement to select all location data
 		String sql = "SELECT * FROM locations WHERE id = '" + locationID + "' LIMIT 1";
+
+        // If selected delete
+        if(deleteChoose != "" && deleteChoose != null)
+        {
+            sql = "DELETE FROM locations WHERE id = '" + locationID + "'";
+        }
 
         // Connect to MySQL
         MysqlConnect mysqlConnect = new MysqlConnect();
@@ -32,19 +40,28 @@ public class EditLocation extends HttpServlet {
 		{
 			// Try statement
 			PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
-			
-			// Loop through statement
-			ResultSet rs = statement.executeQuery();
 
-			while(rs.next())
-			{
-                // Send data back
-                request.setAttribute("id", rs.getString(1));
-                request.setAttribute("iconid", rs.getString(2));
-                request.setAttribute("address", rs.getString(3));
-                request.setAttribute("name", rs.getString(4));
-                request.setAttribute("zip", rs.getString(5));
-			}
+            // If selected edit
+            if(editChoose != "" && editChoose != null)
+            {
+                // Loop through statement
+                ResultSet rs = statement.executeQuery();
+
+                while(rs.next())
+                {
+                    // Send data back
+                    request.setAttribute("id", rs.getString(1));
+                    request.setAttribute("iconid", rs.getString(2));
+                    request.setAttribute("address", rs.getString(3));
+                    request.setAttribute("name", rs.getString(4));
+                    request.setAttribute("zip", rs.getString(5));
+                }
+            }
+            else
+            {
+                // Execute delete
+			    statement.executeUpdate();
+            }
 
             // Redirect user
             RequestDispatcher dispatcher = request.getRequestDispatcher("admin.jsp");
