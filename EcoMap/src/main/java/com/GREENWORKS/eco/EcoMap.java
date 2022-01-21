144b3415d8b5cff5c0ab9fbbfc591d5748aa6e46
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2022-01-20 21:59:09 -0500

    Correction

--- EcoMap/src/main/java/com/GREENWORKS/eco/EcoMap.java
+++ EcoMap/src/main/java/com/GREENWORKS/eco/EcoMap.java
@@ -58,6 +58,8 @@ public class EcoMap {
 			returnArray.add(rs.getString(3));
 			returnArray.add(rs.getString(4));
 			returnArray.add(rs.getString(5));
+			returnArray.add(rs.getString(6));
+			returnArray.add(rs.getString(7));
 		}
 		mysqlConnect.disconnect();
 		return returnArray;		
