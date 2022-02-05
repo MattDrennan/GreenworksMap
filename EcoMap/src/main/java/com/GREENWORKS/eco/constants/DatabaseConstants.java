b3bff271afe360d5a9dc1941b05f9b6f4f0692b7
EthanNValencia <ejnephew@yahoo.com>
2022-02-05 10:44:11 -0500

    Moving DatabaseConstants to the constants package.

--- EcoMap/src/main/java/com/GREENWORKS/eco/constants/DatabaseConstants.java
+++ EcoMap/src/main/java/com/GREENWORKS/eco/constants/DatabaseConstants.java
@@ -11,8 +11,8 @@ package com.GREENWORKS.eco.constants;
  */
 public class DatabaseConstants {
     public static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/ecomap"; 
-    public static final String USERNAME = "RESTuser";
-    public static final String PASSWORD = "Feiasjda";
+    public static final String DATABASE_URL = "jdbc:mysql://localhost:8889/ecomap"; 
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
     public static final String MAX_POOL = "250";
 }
