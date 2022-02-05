23f20614b4be3a64a3295b84cace5599c3d981f2
EthanNValencia <ejnephew@yahoo.com>
2022-02-05 10:40:26 -0500

    Moving DatabaseConstants to the constants package.

---
+++ EcoMap/src/main/java/com/GREENWORKS/eco/constants/DatabaseConstants.java
@@ -0,0 +1,18 @@
+package com.GREENWORKS.eco.constants;
+
+/***
+ * This class is for housing the database constants that we will use during project development. It 
+ * will be necessary for this file to be modified by team members on their remote machines. Constants 
+ * that will probably need to be modified are the USERNAME, PASSWORD, and DATABASE_URL. 
+ * 
+ * DATABASE_URL: MySQL Workbench uses port number 3306 by default. 
+ * USERNAME: Change this to your database username. 
+ * PASSWORD: Change this to your database password.
+ */
+public class DatabaseConstants {
+    public static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/ecomap"; 
+    public static final String USERNAME = "RESTuser";
+    public static final String PASSWORD = "Feiasjda";
+    public static final String MAX_POOL = "250";
+}
